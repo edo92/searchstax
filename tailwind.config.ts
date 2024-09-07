160d00aa448a371ec528283e6a7f5b8e20e174b2
edo92 <edoter92@gmail.com>
2024-09-07 08:34:35 -0700

    update styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,33 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        light: {
+          100: "rgb(var(--color-light-100) / <alpha-value>)",
+        },
+
+        dark: {
+          100: "rgb(var(--color-dark-100) / <alpha-value>)",
+          200: "rgb(var(--color-dark-200) / <alpha-value>)",
+        },
       },
     },
   },
+
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          background: "var(--color-dark-100)",
+        },
+      },
+      {
+        light: {
+          background: "var(--color-light-100)",
+        },
+      },
+    ],
+  },
+
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
 export default config;
