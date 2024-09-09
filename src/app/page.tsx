52c5badc6df16dff572af47797a6e594f1d77939
edo92 <edoter92@gmail.com>
2024-09-09 07:05:26 -0700

    add sorting template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,66 @@
 "use client";
 
-import { SearchstaxInputWidget } from "@searchstax-inc/searchstudio-ux-react";
-import { renderConfig } from "./config";
+import {
+  SearchstaxWrapper,
+  SearchstaxInputWidget,
+  SearchstaxOverviewWidget,
+  SearchstaxFacetsWidget,
+} from "@searchstax-inc/searchstudio-ux-react";
+import type { Searchstax } from "@searchstax-inc/searchstudio-ux-js";
+import { env } from "@/env";
 
-import { SearchWrapper } from "./components/SearchWrapper";
+import { renderConfig } from "./config";
 import { InputTemplate } from "./components/SearchTemplates/Input.Template";
+import { useRef } from "react";
+import { OverviewTemplate } from "./components/SearchTemplates/Overview.Template";
+import { FacetDesktopTemplate } from "./components/SearchTemplates/Facet.Template";
 
 export default function Home() {
+  const instanceRef = useRef<Searchstax>();
+
+  const handleInitialized = (instance: Searchstax) => {
+    console.log("Searchstax initialized");
+    instanceRef.current = instance;
+  };
+
   return (
-    <SearchWrapper>
+    <SearchstaxWrapper
+      searchURL={env.NEXT_PUBLIC_SEARCH_URL}
+      suggesterURL={env.NEXT_PUBLIC_SUGGESTER_URL}
+      trackApiKey={env.NEXT_PUBLIC_TRACK_API_KEY}
+      searchAuth={env.NEXT_PUBLIC_SEARCH_AUTH}
+      authType={env.NEXT_PUBLIC_AUTH_TYPE}
+      initialized={handleInitialized}
+    >
       <div className="searchstax-page-layout-container">
-        <SearchstaxInputWidget
-          inputTemplate={InputTemplate}
-          suggestAfterMinChars={renderConfig.inputWidget.suggestAfterMinChars}
-        />
+        <div className="px-7 py-5">
+          <SearchstaxInputWidget
+            inputTemplate={(sugg, leave, over, click) =>
+              InputTemplate(sugg, leave, over, click, instanceRef.current!)
+            }
+            suggestAfterMinChars={renderConfig.inputWidget.suggestAfterMinChars}
+          />
+
+          <div className="search-details-container">
+            <SearchstaxOverviewWidget
+              searchOverviewTemplate={OverviewTemplate}
+            />
+          </div>
+
+          <div className="flex flex-row gap-5">
+            <SearchstaxFacetsWidget
+              facetingType={renderConfig.facetsWidget.facetingType}
+              itemsPerPageMobile={renderConfig.facetsWidget.itemsPerPageMobile}
+              itemsPerPageDesktop={
+                renderConfig.facetsWidget.itemsPerPageDesktop
+              }
+              facetsTemplateDesktop={FacetDesktopTemplate}
+              // facetsTemplateMobile={facetsTemplateMobile}
+              specificFacets={undefined}
+            />
+          </div>
+        </div>
       </div>
-    </SearchWrapper>
+    </SearchstaxWrapper>
   );
 }
