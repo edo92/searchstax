b9f2cf1226302b7075191b1e573d40f541eff6db
edo92 <edoter92@gmail.com>
2024-09-09 06:57:22 -0700

    add overview template

---
+++ src/app/components/SearchTemplates/Overview.Template.tsx
@@ -0,0 +1,60 @@
+import { ISearchstaxSearchFeedbackData } from "@searchstax-inc/searchstudio-ux-js";
+
+export function OverviewTemplate(
+  searchFeedbackData: null | ISearchstaxSearchFeedbackData,
+  onOriginalQueryClick: (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => void
+) {
+  if (
+    !searchFeedbackData ||
+    !searchFeedbackData.searchExecuted ||
+    searchFeedbackData.totalResults <= 0
+  ) {
+    return <></>;
+  }
+
+  const {
+    startResultIndex,
+    endResultIndex,
+    totalResults,
+    searchTerm,
+    autoCorrectedQuery,
+    originalQuery,
+  } = searchFeedbackData;
+
+  return (
+    <div className="mb-3">
+      <div className="text-dark-200 dark:text-light-200 flex flex-col">
+        <div className="flex flex-row gap-x-1">
+          <small>Showing</small>
+          <small>
+            <b>{`${startResultIndex} - ${endResultIndex}`}</b>
+          </small>
+          <small>of</small>
+          <small>
+            <b>{totalResults}</b> results
+          </small>
+          {searchTerm && (
+            <small>
+              for <b>&quot;{searchTerm}&quot;</b>
+            </small>
+          )}
+        </div>
+
+        {autoCorrectedQuery && (
+          <div className="searchstax-feedback-container-suggested">
+            Search instead for{" "}
+            <a
+              href="#"
+              onClick={onOriginalQueryClick}
+              className="searchstax-feedback-original-query"
+            >
+              {originalQuery}
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
